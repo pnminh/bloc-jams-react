53626f8ef9c297140bd673f9eea03e5bb7ac6d72
Mai Huynh <nhumaihuynh91@gmail.com>
2018-09-14 18:34:53 -0500

    Audio Playback Checkpoint: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,21 +10,51 @@ class Album extends Component{
     });
 
     this.state = {
-        album2: album
+        album: album,
+        currentSong: album.songs[0],
+        isPlaying: false
+    };
+
+    this.audioElement = document.createElement("audio");
+    this.audioElement.src = album.songs[0].audioSrc;
     }
 
+    play() {
+        this.audioElement.play();
+        this.setState({isPlaying: true});
     }
-    render(){
+
+    pause() {
+        this.audioElement.pause();
+        this.setState({isPlaying: false})
+    }
+
+    setSong(song){
+        this.audioElement.src = song.audioSrc;
+        this.setState({currentSong: song});
+    }
+
+    handleSongClick(song){
+        const isSameSong = this.state.currentSong === song;
+        if (this.state.isPlaying && isSameSong){
+            this.pause()
+        } else{
+            if (!isSameSong) { this.setSong(song); } 
+            this.play();
+        }
+    }
+
+    render() {
         return(
             <section className="album">
                 <section id="album-info">
 
-                <img id="album-cover-art" src={this.state.album2.albumCover} alt={this.state.album2.title}/>
+                <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
 
                 <div className="album-details">
-                    <h1 id="album-title">{this.state.album2.title}</h1>
-                    <h2 className="artist">{this.state.album2.artist}</h2>
-                    <div id="release-info">{this.state.album2.releaseInfo}</div>
+                    <h1 id="album-title">{this.state.album.title}</h1>
+                    <h2 className="artist">{this.state.album.artist}</h2>
+                    <div id="release-info">{this.state.album.releaseInfo}</div>
                 </div>
                 
                 </section>
@@ -38,11 +68,11 @@ class Album extends Component{
 
                   <tbody>
                       {
-                          this.state.album2.songs.map((albu, index) =>
-                        <tr key = {index}>
+                          this.state.album.songs.map((song, index) =>
+                          <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                             <td>{index +1} </td> 
-                            <td>{albu.title}</td>
-                            <td>{albu.duration} </td>
+                            <td>{song.title}</td>
+                            <td>{song.duration} </td>
                         </tr>
                        
                     )
